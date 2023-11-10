8cfdc477caca3c35e34fb1519d3271e260e48f06
Sarah Gephart <sgephart@nic.edu>
2023-11-09 23:43:13 -0800

    Commit Experiments 11/9/23

---
+++ script.js
@@ -0,0 +1,9 @@
+const navBg = document.querySelector('.navBg');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    navBg.classList.toggle('active');
+})
+
+
+// console.log('Hello World');
